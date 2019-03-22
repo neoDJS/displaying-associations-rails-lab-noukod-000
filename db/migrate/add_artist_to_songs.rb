e9f3210e0c48df843fa0c951f0fb002e87ae3f22
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-22 19:23:53 +0000

    Automatically backed up by Learn

--- db/migrate/add_artist_to_songs.rb
+++ db/migrate/add_artist_to_songs.rb
@@ -1,9 +1,5 @@
 class AddArtistToSongs < ActiveRecord::Migration
   def change
-    create_table :artists do |t|
-      t.string :name
-
-      t.timestamps null: false
-    end
+    add_column :songs, :artist_id, :integer
   end
 end
